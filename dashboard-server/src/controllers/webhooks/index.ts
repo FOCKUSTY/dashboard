0efd8365ca109541f48923a31f738ef0cca7d806
FOCKUSTY <wwddwwwd@mail.ru>
2024-05-29 13:46:23 +0500

    added post to webhook

--- dashboard-server/src/controllers/webhooks/index.ts
+++ dashboard-server/src/controllers/webhooks/index.ts
@@ -1,5 +1,33 @@
 import { Response, Request } from "express"
 import { getWebhookService } from "../../services/webhooks"
+import { WebhookClient } from "discord.js";
+
+export async function postWebhookController(req: Request, res: Response)
+{
+    try
+    {
+        const webhookData = await getWebhookService(req.params.webhookId, req.params.webhookToken);
+        const avatarsrc = `https://cdn.discordapp.com/avatars/${webhookData.id}/${webhookData.avatar}`
+        const data: any = {
+            avatarURL: req.body.avatar_url || avatarsrc,
+            username: req.body.name || webhookData.name,
+            content: req.body.content || null,
+            components: req.body.components || null,
+            embed: req.body.embed || null,
+            poll: req.body.poll || null
+        };
+
+        const webhook = new WebhookClient({id: req.params.webhookId, token: req.params.webhookToken});
+        const sendedMessageData = await webhook.send(data);
+
+        res.send(sendedMessageData);
+    }
+    catch (err)
+    {
+        console.error(err);
+        res.status(400).send({msg: 'Error'});
+    };
+};
 
 export async function getWebhookController(req: Request, res: Response)
 {
--- dashboard-server/src/routes/webhooks/index.ts
+++ dashboard-server/src/routes/webhooks/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { isAuthenticated } from '../../utils/middlewares'
-import { getWebhookController } from "../../controllers/webhooks";
+import { getWebhookController, postWebhookController } from "../../controllers/webhooks";
 
 const router = Router();
 
 router.get('/:webhookId', isAuthenticated, getWebhookController);
-router.get('/:webhookId/:webhookToken', isAuthenticated, getWebhookController);
+router.get('/:webhookId/:webhookToken', getWebhookController);
+router.post('/:webhookId/:webhookToken', postWebhookController);
 
 export default router;
