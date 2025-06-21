04242a31e127ebbf47ed4a9e5b32bae08dec197b
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-06-21 10:15:23 +0500

    -style

--- backend/settings/compiler.ts
+++ backend/settings/compiler.ts
@@ -4,9 +4,10 @@ import { Settings } from "./settings";
 
 new Compiler(
   Object.fromEntries(
-    Object.keys(Settings.CONSTANTS.object.available)
-      .map(key => [
-        key, Object.keys(Settings.CONSTANTS.object.available[key]
-  )])),
+    Object.keys(Settings.CONSTANTS.object.available).map((key) => [
+      key,
+      Object.keys(Settings.CONSTANTS.object.available[key])
+    ])
+  ),
   __dirname + "\\format.settings.ts"
 ).execute();
