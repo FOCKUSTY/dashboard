333feac0d51dbd4f0b2a4edd965c789c086b643d
fockusty <wwddwww@mail.ru>
2025-07-18 10:26:36 +0500

    -up auth strategy

--- apps/backend/src/strategies/authenticator.ts
+++ apps/backend/src/strategies/authenticator.ts
@@ -6,21 +6,17 @@ import { Profile } from "passport";
 import { AuthTypes, IAuthUser } from "types/auth-user.type";
 import { Strategy, VerifyCallback, VerifyFunction } from "passport-oauth2";
 
-import { getPassportAuthEnv } from "src/api";
+import Api, { getPassportAuthEnv } from "src/api";
 import { IUser } from "types/user.type";
 import { Model } from "mongoose";
+import { APIUser } from "discord.js";
 
+const { env } = new Api.env();
 const { Auth, User } = MODELS;
 
-/**
- * @types [AuthTypes, string, string[]?] (first, second, third)
- * @first the method of authentication
- * @second the module in passport
- * @third a scopes
- */
-const defaultPassports: [AuthTypes, string, string[]?][] = [
-  ["discord", "passport-discord", ["identify", "email", "guilds"]]
-];
+const defaultPassports: Record<AuthTypes, {path: string, scopes?: string[]}> = {
+  discord: { path: "passport-discord", scopes: ["identify", "email", "guilds"] }
+}
 
 const CreateOrUpdate = async <T>({ model, findData, data }: {model: Model<T>, findData: Partial<T>, data: Partial<T>}) => {
   const finded = await model.findOne(findData);
@@ -41,14 +37,14 @@ class Authenticator {
   }
 
   public init = () => {
-    for (const passport of defaultPassports) {
-      const strategy = require(passport[1]).Strategy;
+    for (const passport in defaultPassports) {
+      const strategy = require(defaultPassports[passport].path).Strategy;
       this.strategy(strategy, {
         ...getPassportAuthEnv(
-          passport[0].toUpperCase() as Uppercase<AuthTypes>
+          passport.toUpperCase() as Uppercase<AuthTypes>
         ),
-        type: passport[0],
-        scopes: passport[2]
+        type: defaultPassports[passport].path,
+        scopes: defaultPassports[passport]?.scopes || []
       });
     }
   };
@@ -68,7 +64,23 @@ class Authenticator {
 
         const username = profile.username || profile.displayName || profile.name.givenName;
 
-        const user = (await CreateOrUpdate({ model: User, findData: { username }, data: { created_at: now }})).toObject();
+        let user: IUser;
+        try {
+          const apiUser: APIUser = await (await fetch(env.DISCORD_API_URL + "/users/@me", {
+            method: "GET",
+            headers: { Authorization: "Bearer " + access_token }
+          })).json();
+          
+          user = (await CreateOrUpdate({ model: User, findData: { username }, data: {
+            created_at: now,
+            nickname: apiUser.global_name,
+            // ВЫНЕСТИ В КОНСТАНТУ
+            avatar_url: "https://cdn.discordapp.com/avatars/" + apiUser.id + "/" + apiUser.avatar + ".webp"
+          }})).toObject();
+        } catch {
+          user = (await CreateOrUpdate({ model: User, findData: { username }, data: { created_at: now }})).toObject();
+        }
+
         const auth = (await CreateOrUpdate({ model: Auth, findData: { service_id: id }, data: {
           profile_id: user.id,
     
