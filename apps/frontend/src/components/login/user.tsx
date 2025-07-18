730cd6bbe56ad52f08e177e13709dc6a1f7e00f4
fockusty <wwddwww@mail.ru>
2025-07-18 11:30:20 +0500

    -fix styles

--- apps/frontend/src/components/login/user.tsx
+++ apps/frontend/src/components/login/user.tsx
@@ -17,20 +17,18 @@ const AvatarUser = ({ user }: { user: IUser }) => {
 
 export const User = ({ user }: { user: IUser }) => {
   return (
-    <div>
-      <Dropdown className={`${styles.dropdown} noselect`} id="user-dropdown" summary={<AvatarUser user={user}/>}>
-        <span>{user.nickname || user.username}</span>
-        <button onClick={() => {
-          logOut().then(() => {
-            location.reload();
-          });
-        }}>Выйти</button>
-        <button onClick={() => {
-          deleteSession().then(() => {
-            location.reload();
-          });
-        }}>Удалить сессию</button>
-      </Dropdown>
-    </div>
+    <Dropdown className={`${styles.dropdown} noselect`} id="user-dropdown" summary={<AvatarUser user={user}/>}>
+      <span>{user.nickname || user.username}</span>
+      <button onClick={() => {
+        logOut().then(() => {
+          location.reload();
+        });
+      }}>Выйти</button>
+      <button onClick={() => {
+        deleteSession().then(() => {
+          location.reload();
+        });
+      }}>Удалить сессию</button>
+    </Dropdown>
   );
 };
