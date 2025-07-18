07047b56e27b69739d71be330914b93e6f92ede9
fockusty <wwddwww@mail.ru>
2025-07-18 11:32:39 +0500

    -up user fetching

--- apps/frontend/src/api/fetch-user.ts
+++ apps/frontend/src/api/fetch-user.ts
@@ -1,10 +1,18 @@
 "use server";
 
+import { cookies } from "next/headers";
+
 import { IUser } from "types/user.type";
 import { Api } from "api";
 
 export const fetchUser = async (token: string): Promise<IUser | null> => {
   try {
+    const fromCookie = (await cookies()).get("user");
+    
+    if (fromCookie) {
+      return JSON.parse(fromCookie.value) as IUser
+    };
+
     const user = await fetch(Api.url + "/users", {
       method: "GET",
       headers: { authentication: token }
