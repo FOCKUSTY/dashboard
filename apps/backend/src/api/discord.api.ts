cff84c41a06555cd67072127148270102c8f4349
fockusty <wwddwww@mail.ru>
2025-07-21 15:30:53 +0500

    -up discord api

--- apps/backend/src/api/discord.api.ts
+++ apps/backend/src/api/discord.api.ts
@@ -15,15 +15,15 @@ export class DiscordApi {
 
   public static getBotAuth() {
     return { Authorization: 'Bot ' + this._token };
-  };
+  }
 
   public static getUserAuth(token: string) {
     return { Authorization: 'Bearer ' + token };  
-  };
+  }
 
   public static fetchUserAvatar(user: { id: string, avatar: string}) {
     return `${this.cdn}/avatar/${user.id}/${user.avatar}.webp`;
-  };
+  }
 
   public static async fetchUserGuilds(token: string): Promise<IResponse<APIPartialGuild[], APIPartialGuild[]>> {
     try {
@@ -38,7 +38,37 @@ export class DiscordApi {
     } catch (error) {
       return unknownError.execute(1, <const>[], error);
     }
-  };
+  }
+
+  public static async fetchBotGuilds(): Promise<IResponse<APIPartialGuild[], APIPartialGuild[]>> {
+    try {
+      return {
+        data: await (await fetch(`${this.url}/users/@me/guilds?limit=20`, {
+          method: "GET",
+          headers: this.getBotAuth()
+        })).json(),
+        error: null,
+        successed: true
+      };
+    } catch (error) {
+      return unknownError.execute(1, <const>[], error);
+    }
+  }
+
+  private static intersectionService<T>(first: T[], second: T[], type: "intersection"|"division") {
+    return first.filter(element => type === "intersection"
+      ? second.includes(element)
+      : !second.includes(element)
+    );
+  }
+
+  public static findGuildsIntersection(guilds: {user: string[], bot: string[]}) {
+    return this.intersectionService(guilds.bot, guilds.user, "intersection");
+  }
+  
+  public static findGuildsDivision(guilds: {user: string[], bot: string[]}) {
+    return this.intersectionService(guilds.bot, guilds.user, "division");
+  }
 };
 
 export default DiscordApi;
