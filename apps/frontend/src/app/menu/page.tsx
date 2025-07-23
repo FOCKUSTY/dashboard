05b00332b09b8bbc53eef181b6582ea8a1f29366
fockusty <wwddwww@mail.ru>
2025-07-23 22:36:58 +0500

    -add /menu page

--- apps/frontend/src/app/menu/page.tsx
+++ apps/frontend/src/app/menu/page.tsx
@@ -2,12 +2,27 @@
 
 import styles from "./page.module.css";
 
-import { useEffect, useState } from "react";
-
 import Image from "next/image";
+
+import { useEffect, useState } from "react";
 
 import { validateCookies } from "api/validate-cookies";
 import { fetchGuilds } from "api/fetch-guilds";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Mousewheel, Pagination } from "swiper/modules";
+
+const resolveGuilds = (guilds: any[]) => {
+  if (guilds.length >= 8) {
+    return guilds
+  } else {
+    return [
+      ...guilds,
+      ...guilds.map(guild => { return { ...guild, id: (+(guild.id + guilds.length)*2) }}),
+      ...guilds.map(guild => { return { ...guild, id: (+(guild.id + guilds.length)*3) }}),
+    ]
+  };
+};
 
 export const Page = () => {
   const [ guilds, setGuilds ] = useState<{id: string, name: string, icon_url: string|null, banner_url: string|null}[]|null>(null);
@@ -31,15 +46,41 @@ export const Page = () => {
   return (
     <div id={styles.main}>
       <div id={styles.menu}>
-        {
-          guilds.map(guild =>
-            <div key={guild.id} className={styles.card}>
-              <Image height={50} width={50} src={guild.icon_url || "/TheVoidAvatarSite.png"} alt={guild.name + " icon"}/>
-              <span>{guild.name}</span>
-            </div>
-            // <span key={guild.id}>{guild.name}</span>
-          )
-        }
+        <Swiper
+          direction={"vertical"}
+          slidesPerView={5}
+          centeredSlides={true}
+          coverflowEffect={{
+            rotate: 30,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            scale: 0.925,
+            slideShadows: true
+          }}
+          mousewheel={{
+            enabled: true,
+            thresholdDelta: 70
+          }}
+          effect="coverflow"
+          pagination={{ clickable: true, }}
+          spaceBetween={30}
+          modules={[Mousewheel, Pagination, EffectCoverflow]}
+          loop={true}
+          keyboard={true}
+          className={`${styles.swiper} mySwiper mousewheel-control-swiper rounded`}
+        >
+          <div className="swiper-wrapper">
+            {
+              resolveGuilds(guilds).map(guild =>
+                <SwiperSlide key={guild.id} className={styles.card}>
+                  <Image height={50} width={50} src={guild.icon_url || "/TheVoidAvatarSite.png"} alt={guild.name + " icon"}/>
+                  <span>{guild.name}</span>
+                </SwiperSlide>
+              )
+            }
+          </div>
+        </Swiper>
       </div>
     </div>
   )
