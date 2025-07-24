1c5abb7583ae23c6f22b44dc04c7083cc5ea999f
fockusty <wwddwww@mail.ru>
2025-07-24 14:49:50 +0500

    -ref code > menu > page.tsx

--- apps/frontend/src/app/menu/page.tsx
+++ apps/frontend/src/app/menu/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 
 import Image from "next/image";
 
-import { useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 
 import { validateCookies } from "api/validate-cookies";
 import { fetchGuilds } from "api/fetch-guilds";
@@ -27,6 +27,16 @@ const resolveGuilds = (guilds: any[]) => {
   };
 };
 
+const Parent = ({ children, style }: { children: React.ReactNode, style?: CSSProperties }) => {
+  return (
+    <div id={styles.main}>
+      <div id={styles.menu} style={style}>
+        {children}
+      </div>
+    </div>
+  )
+}
+
 export const Page = () => {
   const router = useRouter();
   const [ guilds, setGuilds ] = useState<ICardGuild[]|null>(null);
@@ -45,74 +55,68 @@ export const Page = () => {
 
   if (!guilds || guilds.length === 0) {
     return (
-      <div id={styles.main}>
-        <div id={styles.menu} style={{flexDirection: "column", margin: "0 0 0 30%"}}>
-          <span>Мы не нашли у Вас серверов, но Вы всегда можете пригласить нас!</span>
-          <button onClick={() => (window.location.href = Api.invite_url)}>
-            Пригласить The Voidy
-          </button>
-        </div>
-      </div>
-    );
+      <Parent style={{flexDirection: "column", margin: "0 0 0 30%"}}>
+        <span>Мы не нашли у Вас серверов, но Вы всегда можете пригласить нас!</span>
+        <button onClick={() => (window.location.href = Api.invite_url)}>
+          Пригласить The Voidy
+        </button>
+      </Parent>
+    )
   };
 
   if (guilds.length <= 2) {
     return (
-      <div id={styles.main}>
-        <div id={styles.menu} style={{flexDirection: "column", margin: "0 0 0 30%"}}>
-          {
-            guilds.map(guild =>
-              <div key={guild.id} className={styles.card} onClick={() => router.push("/dashboard/"+guild.id)}>
-                <Image height={50} width={50} src={guild.icon_url || "/TheVoidAvatarSite.png"} alt={guild.name + " icon"}/>
-                <span>{guild.name}</span>
-              </div>
-            )
-          }
-        </div>
-      </div>
+      <Parent style={{flexDirection: "column", margin: "0 0 0 30%"}}>
+        {
+          guilds.map(guild =>
+            <div key={guild.id} className={styles.card} onClick={() => router.push("/dashboard/"+guild.id)}>
+              <Image height={50} width={50} src={guild.icon_url || "/TheVoidAvatarSite.png"} alt={guild.name + " icon"}/>
+              <span>{guild.name}</span>
+            </div>
+          )
+        }
+      </Parent>
     )
   }
 
   return (
-    <div id={styles.main}>
-      <div id={styles.menu}>
-        <Swiper
-          direction={"vertical"}
-          slidesPerView={5}
-          centeredSlides={true}
-          coverflowEffect={{
-            rotate: 30,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            scale: 0.925,
-            slideShadows: true
-          }}
-          mousewheel={{
-            enabled: true,
-            thresholdDelta: 70
-          }}
-          effect="coverflow"
-          pagination={{ clickable: true, }}
-          spaceBetween={30}
-          modules={[Mousewheel, Pagination, EffectCoverflow]}
-          loop={true}
-          keyboard={true}
-          className={`${styles.swiper} mySwiper mousewheel-control-swiper rounded`}
-        >
-          <div className="swiper-wrapper">
-            {
-              resolveGuilds(guilds).map(guild =>
-                <SwiperSlide key={guild.id} className={styles.card} onClick={() => router.push("/dashboard/"+guild.id)}>
-                  <Image height={50} width={50} src={guild.icon_url || "/TheVoidAvatarSite.png"} alt={guild.name + " icon"}/>
-                  <span>{guild.name}</span>
-                </SwiperSlide>
-              )
-            }
-          </div>
-        </Swiper>
-      </div>
-    </div>
+    <Parent>
+      <Swiper
+        direction={"vertical"}
+        slidesPerView={5}
+        centeredSlides={true}
+        coverflowEffect={{
+          rotate: 30,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          scale: 0.925,
+          slideShadows: true
+        }}
+        mousewheel={{
+          enabled: true,
+          thresholdDelta: 70
+        }}
+        effect="coverflow"
+        pagination={{ clickable: true, }}
+        spaceBetween={30}
+        modules={[Mousewheel, Pagination, EffectCoverflow]}
+        loop={true}
+        keyboard={true}
+        className={`${styles.swiper} mySwiper mousewheel-control-swiper rounded`}
+      >
+        <div className="swiper-wrapper">
+          {
+            resolveGuilds(guilds).map(guild =>
+              <SwiperSlide key={guild.id} className={styles.card} onClick={() => router.push("/dashboard/"+guild.id)}>
+                <Image height={50} width={50} src={guild.icon_url || "/TheVoidAvatarSite.png"} alt={guild.name + " icon"}/>
+                <span>{guild.name}</span>
+              </SwiperSlide>
+            )
+          }
+        </div>
+      </Swiper>
+    </Parent>
   )
 };
 
