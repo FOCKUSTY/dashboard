730bc0a4504b4490037ee9c400a4bc36c52fc890
fockusty <wwddwww@mail.ru>
2025-07-26 10:56:24 +0500

    -up links

--- apps/frontend/src/api/index.ts
+++ apps/frontend/src/api/index.ts
@@ -5,7 +5,8 @@ export class Api {
   public static readonly the_void = {
     github_url: "https://github.com/The-Void-Community",
     discord_url: "https://discord.gg/97J8mnn4Gr",
-    telegram_url: "https://t.me/The_Void_Community"
+    telegram_url: "https://t.me/The_Void_Community",
+    site: "https://fockusty.netlify.app"
   } as const;
 
   public static readonly invite_url = `https://discordapp.com/api/oauth2/authorize?client_id=1122199797449904179&redirect_url=${encodeURIComponent(`${Api.url}/auth/discord`)}&response_type=code`;
