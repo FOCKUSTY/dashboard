72d9500410e8990314f8271977f4cd2a922ae28e
fockusty <wwddwww@mail.ru>
2025-08-07 18:48:24 +0500

    -add roles fetching

--- apps/frontend/src/api/fetch-guild-data.ts
+++ apps/frontend/src/api/fetch-guild-data.ts
@@ -2,7 +2,28 @@ import { cache } from "react";
 
 import { Api } from "api";
 
-import { APIWebhook } from "discord.js";
+import { APIRole, APIWebhook } from "discord.js";
+
+export const fetchRoles = (async (
+  token: string,
+  id: string
+): Promise<APIRole[] | null> => {
+  try {
+    const roles = await fetch(Api.url + "/guilds/" + id + "/roles", {
+      method: "GET",
+      headers: { authentication: token },
+      next: {
+        revalidate: 1,
+      },
+      cache: "force-cache"
+    });
+
+    return (await roles.json()).data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+});
 
 export const fetchWebhooks = cache(async (
   token: string,
--- apps/frontend/src/app/dashboard/[guildId]/settings/page.tsx
+++ apps/frontend/src/app/dashboard/[guildId]/settings/page.tsx
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 
-import { fetchWebhooks } from "api/fetch-webhooks";
+import { fetchRoles, fetchWebhooks } from "api/fetch-guild-data";
 import { fetchGuild } from "api/fetch-guilds";
 import { fetchUser } from "api/fetch-user";
 import { validateCookies } from "api/validate-cookies";
@@ -13,9 +13,10 @@ import { validateCookies } from "api/validate-cookies";
 import type { IGuild } from "types/guild.type";
 import type { IUser } from "types/user.type";
 import type { IConfig } from "types/config.type";
-import type { APIWebhook } from "discord.js";
+import type { APIRole, APIWebhook } from "discord.js";
 
 import { Dropdown } from "components/dropdown";
+import { Api } from "api";
 
 const settings: {
   guild: [keyof IConfig["guild"], string][];
@@ -48,25 +49,25 @@ const settings: {
 const SettingsComponent = ({
   main,
   name,
-  addWebhook,
-  webhooks
+  addData,
+  data
 }: {
   main: string,
   name: string,
-  webhooks: APIWebhook[],
-  addWebhook: (id: string, key: string) => void
+  data: { webhooks: APIWebhook[], roles: APIRole[] },
+  addData: (name: "webhooks" | "roles", id: string, key: string) => void
 }) => {
   if (main === "logging") {
     return (
       <>
         {
-          webhooks.length != 0
+          data.webhooks.length != 0
             ? ( 
               <div className="post-settings">
                 <label htmlFor="">Webhook:</label>
                 <Dropdown id={`webhook__${main}_${name}`} summary={"choose webhook"}>
                   {
-                    webhooks.map(webhook => 
+                    data.webhooks.map(webhook => 
                       webhook.name
                     )
                   }
@@ -84,11 +85,18 @@ const SettingsComponent = ({
   };
 
   if (name === "when_user_join_into_guild_grant_roles") {
+    if (data.roles.length === 0) return <></>;
+
     return (
       <>
         <div className="post-settings">
           <label htmlFor={`${main}_${name}`}>Роли</label>
-          <input className="post-settings" name={`${main}_${name}`} type="text" id={`${main}_${name}`} />
+          <Dropdown id={`roles_${main}_${name}`} summary="choose roles">
+            {
+              data.roles.map(role => role.name)
+            }
+          </Dropdown>
+          {/* <input className="post-settings" name={`${main}_${name}`} type="text" id={`${main}_${name}`} /> */}
         </div>
       </>
     )
@@ -97,7 +105,7 @@ const SettingsComponent = ({
   return (
     <>
       {
-        webhooks.length != 0
+        data.webhooks.length != 0
           ? (
             <div className="post-settings">
               <label htmlFor={`webhook__${main}_${name}`}>Webhook:</label>
@@ -121,26 +129,40 @@ const SettingsComponent = ({
 const Page = () => {
   const [ user, setUser ] = useState<IUser | null>(null);
   const [ guild, setGuild ] = useState<IGuild | null>(null);
-  const [ webhooks, setWebhooks ] = useState<APIWebhook[]>([]);
   const [ project, setProject ] = useState<"guild"|"logging">("guild");
   
-  const [ choosedWebhooks, setChoosedWebhooks ] = useState<{
+  const [ { webhooks, roles }, setDatas ] = useState<{
+    webhooks: APIWebhook[],
+    roles: APIRole[],
+  }>({ webhooks: [], roles: [] });
+  
+  const [ choosedData, setChoosedData ] = useState<Record<"webhooks"|"roles", {
     guild: Partial<Record<keyof IConfig["guild"], string>>,
     logging: Partial<Record<keyof IConfig["logging"], string>>
-  }>({
-    guild: {},
-    logging: {}
+  }>>({
+    roles: {
+      guild: {},
+      logging: {}
+    },
+    webhooks: {
+      guild: {},
+      logging: {}      
+    }
   });
 
   const { guildId } = useParams<{guildId: string}>();
 
-  const addWebhook = (id: string, key: string) => {
-    setChoosedWebhooks({
-      ...choosedWebhooks,
-      [project]: {
-        [key]: id
+  const addData = (name: "webhooks"|"roles", id: string, key: string) => {
+    setChoosedData({
+      ...choosedData,
+      [name]: {
+        [project]: {
+          [key]: {
+            id
+          }
+        }
       }
-    });
+    })
   };
 
   useEffect(() => {
@@ -153,8 +175,19 @@ const Page = () => {
 
       if (guildId) {
         setGuild(await fetchGuild(token, guildId));
-        setWebhooks(await fetchWebhooks(token, guildId) || []);
+        setDatas({
+          webhooks: await fetchWebhooks(token, guildId) || [],
+          roles: await fetchRoles(token, guildId) || []
+        });
       };
+
+      fetch(Api.url + "/guilds/" + guildId + "/roles", {
+        method: "GET",
+        headers: { authentication: token }
+      }).then(async (data) => {
+        console.log(await data.json());
+      })
+
       setUser(await fetchUser(token));
     })();
   }, []);
@@ -193,8 +226,8 @@ const Page = () => {
                       main={project}
                       name={key}
                       key={key}
-                      webhooks={webhooks}
-                      addWebhook={addWebhook}
+                      data={{roles, webhooks}}
+                      addData={addData}
                     />
                   </div>
                 );
