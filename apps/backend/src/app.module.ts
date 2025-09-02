7d802c00b9cedbdc57f4876c69b93e71c4bf143c
FOCKUSTY <wwddwwwd@mail.ru>
2025-09-02 19:57:27 +0500

    -added guilds modules

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,15 +5,17 @@ import { CacheModule, CacheInterceptor } from '@nestjs/cache-manager';
 
 import AuthModule from "routes/auth/auth.module";
 import UsersModule from "routes/users/users.module";
+import GuildsModule from "routes/guilds/guilds.module";
 
 @Module({
   imports: [
     ...[
       AuthModule,
-      UsersModule
+      UsersModule,
+      GuildsModule
     ].flatMap((module) => [module, RouterModule.register([{ path: "api", module }])]),
     CacheModule.register({
-      ttl: 5 * 60 * 1000, // 5 minutes
+      ttl: 5 * 60 * 1000 / 1000 / 60 / 5, // 5 minutes
       isGlobal: true
     })
   ],
